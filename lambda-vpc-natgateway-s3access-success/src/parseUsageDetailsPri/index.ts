d04e8b3f701c38003942b6d2253e7e99617ae590
Ganesh Adapa <pransiddu@gmail.com>
2022-02-28 09:40:23 +0000

    Accessing S3 from a private-subnet configured with natgateway.

--- lambda-vpc-natgateway-s3access-success/src/parseUsageDetailsPri/index.ts
+++ lambda-vpc-natgateway-s3access-success/src/parseUsageDetailsPri/index.ts
@@ -15,7 +15,7 @@ export const index = async (event: S3Event) => {
     const client = new S3Client( { region: REGION } );
     const s3GetObjectInput = {
         Bucket,
-        Key: 'CSVFiles/test.csv',
+        Key: 'PrivateCSVFiles/test.csv',
     };
     const getObjectCommand = new GetObjectCommand(s3GetObjectInput);
     const { Body } = await client.send(getObjectCommand);
